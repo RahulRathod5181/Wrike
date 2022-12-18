10f6ce4ed0dc201cbc78dacc77097d7600bda253
RahulRathod5181 <rahulrathod5181@gmail.com>
2022-12-18 06:19:15 +0530

    Day-5 Task

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ let btn = document.getElementById("get");
 btn.addEventListener("click",()=>{
     window.location.href = "sign-up.html"
 })
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+  
